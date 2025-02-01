e5fc48d010be760acfe47892431ffcf1b1e37a04
Samvel Safaryan <whoiam1101@outlook.com>
2025-02-01 21:21:56 +0400

    impl Lion optimizer

---
+++ lion/lion.py
@@ -0,0 +1,70 @@
+import torch
+
+from torch import Tensor
+from torch.optim.optimizer import (
+    Optimizer,
+    ParamsT,
+    _use_grad_for_differentiable
+)
+
+
+__all__ = ["Lion"]
+
+
+class Lion(Optimizer):
+    def __init__(
+        self,
+        params: ParamsT,
+        lr: float | Tensor = 1e-4,
+        betas: tuple[float | Tensor, float | Tensor] = (0.9, 0.999),
+        weight_decay: float = 0.0
+    ):
+        defaults = dict(lr=lr, betas=betas, weight_decay=weight_decay)
+        super().__init__(params, defaults)
+
+    @_use_grad_for_differentiable
+    def step(self, closure: callable | None = None) -> float | None:
+        self._cuda_graph_capture_health_check()
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            beta1, beta2 = group['betas']
+            lr = group['lr']
+            weight_decay = group['weight_decay']
+            decay_factor = 1.0 - lr * weight_decay
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                params_with_grad.append(p)
+                grads.append(p.grad)
+                state = self.state[p]
+                if 'exp_avg' not in state:
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                exp_avgs.append(state['exp_avg'])
+
+            if not params_with_grad:
+                continue
+
+            update_part1 = torch._foreach_mul(exp_avgs, beta1)
+            update_part2 = torch._foreach_mul(grads, 1 - beta1)
+            updates = torch._foreach_add(update_part1, update_part2)
+
+            if decay_factor != 1.0:
+                torch._foreach_mul_(params_with_grad, decay_factor)
+
+            sign_updates = [u.sign() for u in updates]
+            torch._foreach_add_(params_with_grad, sign_updates, alpha=-lr)
+
+            torch._foreach_mul_(exp_avgs, beta2)
+            grad_part = torch._foreach_mul(grads, 1 - beta2)
+            torch._foreach_add_(exp_avgs, grad_part)
+
+        return loss
