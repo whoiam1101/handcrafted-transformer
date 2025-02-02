ad2cf4c2a0efea78ac6e88f54ac775d48ecbc08f
Samvel Safaryan <whoiam1101@outlook.com>
2025-02-02 18:09:46 +0400

    improve

--- transformer/attention.py
+++ transformer/attention.py
@@ -5,9 +5,6 @@ import torch.nn.functional as F
 from torch import Tensor
 
 
-NEG_INF = float(-1e10)
-
-
 def scaled_dot_product_attention(
     query: Tensor,
     key: Tensor,
@@ -17,7 +14,7 @@ def scaled_dot_product_attention(
     d = query.size(-1)
     scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(d)
     if mask is not None:
-        scores.masked_fill_(mask == 0, NEG_INF)
+        scores.masked_fill_(mask == 0, float("-inf"))
     attention_out = F.softmax(scores, dim=-1)
     return torch.matmul(attention_out, value)
 
@@ -28,12 +25,12 @@ class MultiHeadAttention(nn.Module):
         assert d_model % num_heads == 0
 
         self.d_model = d_model
-        self.d = d_model // num_heads
+        self.d_head = d_model // num_heads
         self.num_heads = num_heads
 
-        self.query_fn = nn.Linear(d_model, d_model)
-        self.key_fn = nn.Linear(d_model, d_model)
-        self.value_fn = nn.Linear(d_model, d_model)
+        self.query = nn.Linear(d_model, d_model)
+        self.key = nn.Linear(d_model, d_model)
+        self.value = nn.Linear(d_model, d_model)
         self.final_layer = nn.Linear(d_model, d_model)
 
     def forward(
@@ -45,23 +42,16 @@ class MultiHeadAttention(nn.Module):
     ) -> Tensor:
         batch_size = query.size(0)
 
-        query = (
-            self.query_fn(query)
-            .view(batch_size, -1, self.num_heads, self.d)
-            .transpose(1, 2)
-        )
-        key = (
-            self.key_fn(key)
-            .view(batch_size, -1, self.num_heads, self.d)
-            .transpose(1, 2)
-        )
-        value = (
-            self.value_fn(value)
-            .view(batch_size, -1, self.num_heads, self.d)
-            .transpose(1, 2)
-        )
+        Q = self.query(query)
+        K = self.key(key)
+        V = self.value(value)
+
+        Q = Q.view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)
+        K = K.view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)
+        V = V.view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)
 
         attention_out = scaled_dot_product_attention(query, key, value, mask)
-        attention_out.transpose_(1, 2).contiguous().view_(batch_size, -1, self.d_model)
+        attention_out = attention_out.transpose(1, 2).contiguous()
+        attention_out = attention_out.view(batch_size, -1, self.d_model)
 
         return self.final_layer(attention_out)
