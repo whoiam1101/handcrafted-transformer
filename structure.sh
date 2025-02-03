893154232d5d4a51398a2bf78497cb857ca9991b
Samvel Safaryan <whoiam1101@outlook.com>
2025-02-03 14:04:16 +0400

    make project structure more robust

---
+++ structure.sh
@@ -0,0 +1,7 @@
+mkdir -p ./src/{config,data,models,utils}
+mkdir -p ./{tests,experiments,notebooks,scripts}
+touch ./src/config/{__init__.py,base.py}
+touch ./src/data/{__init__.py,dataset.py,dataloader.py}
+touch ./src/models/{__init__.py,base.py}
+touch ./src/training/{__init__.py,trainer.py,metrics.py,callbacks.py}
+touch ./src/utils/{__init__.py,logger.py}
