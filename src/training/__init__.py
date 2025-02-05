41be6c9a6105b5d85164f8144415030a8d0a3946
Samvel Safaryan <whoiam1101@outlook.com>
2025-02-05 11:42:35 +0400

    __init__.py

--- src/training/__init__.py
+++
@@ -1,2 +0,0 @@
-from .metrics import compute_metrics
-from .trainer import Trainer
--- src/transformer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .transformer import Transformer
-
-# from .encoder import Encoder
-# from .decoder import Decoder
-# from .positional_encoding import PositionalEncoding
-# from .attention import MultiHeadAttention
-# from .swiglu import swiglu
